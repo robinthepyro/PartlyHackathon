59f8f3f4bfe1d4024acabcee039b3f48ef78199e
KokoOCE <79084769+KokoOCE@users.noreply.github.com>
2025-07-27 14:43:42 +1200

    made side frames and buttons

--- gui.py
+++ gui.py
@@ -42,12 +42,23 @@ home_page = ttk.Frame(tabcontrol)
 Q1 = ttk.Frame(tabcontrol)
 Q2 = ttk.Frame(tabcontrol)
 Q3 = ttk.Frame(tabcontrol)
+Q4 = ttk.Frame(tabcontrol)
 
 Result = ttk.Frame(tabcontrol)
 tabcontrol.add(home_page, text="Home")
-tabcontrol.add(Q1, text="Q1")
-tabcontrol.add(Q2, text="Q2")
-tabcontrol.add(Q3, text="Q3")
+tabcontrol.add(Q1, text="FRONT")
+tabcontrol.add(Q2, text="BACK")
+tabcontrol.add(Q3, text="LEFT")
+tabcontrol.add(Q4, text="RIGHT")
+button_start = tk.Button(home_page, text="Start Assessment", command=lambda: tabcontrol.select(print("Assessment Started")))
+button_wind = tk.Button(Q1, text="Windshield", command=lambda: tabcontrol.select(print("Windshield Selected")))
+button_headlights = tk.Button(Q2, text="Headlights", command=lambda: tabcontrol.select(print("Headlights Selected")))
+button_bumper_front = tk.Button(Q3, text="Front Bumper", command=lambda: tabcontrol.select(print("Front Bumper Selected")))
+button_bumper_back = tk.Button(Q4, text="Back Bumper", command=lambda: tabcontrol.select(print))
+button_left_front_door = tk.Button(Q1, text="Left Door", command=lambda: tabcontrol.select(print))
+button_left_back_door = tk.Button(Q2, text="Left Back Door", command=lambda: tabcontrol.select(print("Left Back Door Selected")))
+button_right_front_door = tk.Button(Q3, text="Right Front Door", command=lambda: tabcontrol.select(print("Right Front Door Selected")))
+button_right_back_door = tk.Button(Q2, text="Right Door", command=lambda: tabcontrol.select(print("Right back Door Selected")))
 
 tabcontrol.add(Result, text="Result")
 tabcontrol.pack(expand=1, fill="both")
@@ -64,67 +75,43 @@ center_y = int(screen_height/2 - window_height / 2)
 
 win.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-
-#start button and timer
-def go_to_q1():
-    tabcontrol.select(Q1)
-
-
-tk.Button(
-    home_page,
-    text='START',
-    bg="indian red",
-    fg="black",
-    width=20,
-    height=2,
-    command=go_to_q1
-).place(x=300, y=300)
-
-#selection of options
-def go_to_q2():
-    tabcontrol.select(Q2)
-
-img = Image.open("./assets/part.png")
-img = img.resize((200,200))
-image = ImageTk.PhotoImage(img)
-
-
-button_row = tk.Frame(Q1)
-button_row.pack(pady=20)
-
-car_parts = [
-    {"name": "Hood", "img_path": "./assets/part.png"},
-    {"name": "Bumper", "img_path": "./assets/part.png"},
-    {"name": "Headlight", "img_path": "./assets/part.png"}
-]
-
-images = []
-
-for part in car_parts:
-    img = Image.open(part["img_path"]).resize((100, 100))
-    photo = ImageTk.PhotoImage(img)
-    images.append(photo) 
-
-    # Create the button
-    btn = tk.Button(
-        button_row,
-        image=photo,
-        text=part["name"],
-        compound="top",
-        width=120,
-        height=140,
-        command=go_to_q2
-    )
-    btn.pack(side="left", padx=10)
-
-
-
 # front
 # lights
 # bumper
 # bonnet
 # windscreen
 # what other parts??
+frontimg = Image.open("./assets/front.png")
+backimg = Image.open("./assets/back.png")
+leftimg = Image.open("./assets/leftside.png")
+rightimg = Image.open("./assets/leftside.png")
+
+frontimg = frontimg.resize((300,300))
+backimg = backimg.resize((300,300))
+leftimg = leftimg.resize((300,300))
+rightimg = rightimg.resize((300,300))
+front_image = ImageTk.PhotoImage(frontimg)
+back_image = ImageTk.PhotoImage(backimg)
+left_image = ImageTk.PhotoImage(leftimg)
+right_image = ImageTk.PhotoImage(rightimg)
+front_label = tk.Label(Q1, image=front_image)
+back_label = tk.Label(Q2, image=back_image)
+left_label = tk.Label(Q3, image=left_image)
+right_label = tk.Label(Q4, image=right_image)     
+front_label.pack()
+back_label.pack()
+left_label.pack()
+right_label.pack()
+
+button_wind.pack(pady=20)
+button_headlights.pack(pady=20) 
+button_bumper_front.pack(pady=20)
+button_bumper_back.pack(pady=20)
+button_left_front_door.pack(pady=20)
+button_left_back_door.pack(pady=20)
+button_right_front_door.pack(pady=20)
+button_right_back_door.pack(pady=20)
+button_start.pack(pady=20)
 
 
 # rear
@@ -142,7 +129,11 @@ for part in car_parts:
 # windows
 # what others??
 
-
+img = Image.open("./assets/part.png")
+img = img.resize((300,300))
+image = ImageTk.PhotoImage(img)
+la = tk.Label(home_page, image=image)
+la.pack()
 win.mainloop()
 
 
